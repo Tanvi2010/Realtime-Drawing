2deeb03bc30b8858d8e61faabf8aa9a29157953e
Tanvi <72029176+Tanvi2010@users.noreply.github.com>
2021-03-19 19:38:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,17 @@ poseNet.on("pose",gotPoses);
 
 nosex=0;
 nosey=0;
+leftwristx=0;
+rightwristx=0;
+difference=0;
     
 function draw() {
     background("#8fdfd6");
     fill("#ee95a4");
     stroke("#ee95a4");
-    square(nosex,nosey,100)
+    square(nosex,nosey,difference);
+    document.getElementById("size").innerHTML="square size is"+difference+"px";
+
 }
 
 function modelLoaded() {
@@ -25,5 +30,9 @@ function gotPoses(results) {
       console.log(results);
       nosex=results[0].pose.nose.x;
       nosey=results[0].pose.nose.y;
+      leftwristx=results[0].pose.leftWrist.x;
+      rightwristx=results[0].pose.rightWrist.x;
+      difference=floor(leftwristx-rightwristx);
+      console.log("leftwristx="+leftwristx+"rightwristx="+rightwristx+"difference="+difference);
   }
 }
